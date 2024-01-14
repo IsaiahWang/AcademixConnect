db6ecb6012baf837c3db4c7cac09702bea1c679f
shin-aung <118516554+shin-aung@users.noreply.github.com>
2024-01-14 21:00:02 +0800

    Add files via upload

---
+++ Version 3/registration.php
@@ -0,0 +1,65 @@
+<body>
+<link rel="stylesheet" href="style2.css">
+<div class="container">
+    <div class="title">Registration></div>
+    <form action="#"> 
+        <div class="user-details">
+            <div class="input-box">
+                <sapn class="detail">Full Name</span>
+                <input type="text" placeholder="Enter your name" required>
+            </div>
+            <div class="input-box">
+                <sapn class="detail">Email</span>
+                <input type="text" placeholder="Enter your email" required>
+            </div>
+            <div class="input-box">
+                <sapn class="detail">Phone Number "Please put your country code"</span>
+                <input type="text" placeholder="Enter your phone number" required>
+            </div>
+            <div class="input-box">
+                <sapn class="detail">Course interested in</span>
+                <input type="text" placeholder="Enter the course" required>
+            </div>
+            <div class="input-box">
+                <sapn class="detail">Nationality</span>
+                <input type="text" placeholder="Enter your nationality" required>
+            </div>
+            <div class="input-box">
+                <sapn class="detail">Highest Qualification</span>
+                <input type="text" placeholder="Enter your highest quaification" required>
+            </div>
+            <div class="input-box">
+                <sapn class="detail">Part Time or Full Time</span>
+                <input type="text" placeholder="Enter the time frame" required>
+            </div>
+            <div class="input-box">
+                <sapn class="detail">The Singapore campus of James Cook University can contact me regarding information on other courses, future events and news updates</span>
+                <input type="text" placeholder="Enter Yes or No" required>
+            </div>
+        </div>
+        <div class="gender-details">
+            <input type="radio" name="gender" id="dot-1">
+            <input type="radio" name="gender" id="dot-2">
+            <input type="radio" name="gender" id="dot-3">
+            <span class ="gender-title>Gender">Gender</span>
+            <div class="category">
+                <label for="dot-1">
+                    <span class="dot one"></span>
+                    <span class="gender">Male</span>
+                </label>
+                <label for="dot-2">
+                    <span class="dot two"></span>
+                    <span class="gender">Female</span>
+                </label>
+                <label for="dot-3">
+                    <span class="dot three"></span>
+                    <span class="gender">Prefer not to say</span>
+                </label>
+            </div>
+        </div>
+        <div class="button">
+            <input type="submit" value="Register">
+        </div>
+    </form>
+</div>
+</body>
